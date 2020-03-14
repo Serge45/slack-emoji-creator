47c2dd6d08feb1eee587fcb347d9a7194efdd2ec
Serge45 <Serge45497@gmail.com>
2020-03-14 22:23:46 +0800

    refactor: use generator to slice image

--- slack_emoji_creator/image_slice.py
+++ slack_emoji_creator/image_slice.py
@@ -4,6 +4,16 @@ from PIL import Image, ImageSequence
 
 SLACK_EMOJI_SIZE = (128, 128)
 
+def _get_next_slice(image, window_size):
+    w, h = image.size
+    num_slice_x = w//window_size[0]
+    num_slice_y = h//window_size[1]
+
+    for j in range(num_slice_y):
+        for i in range(num_slice_x):
+            bounds = (i*window_size[0], j*window_size[1], (i+1)*window_size[0], (j+1)*window_size[1])
+            yield image.crop(bounds)
+
 def gif_slice(filepath, 
               output_prefix=None, 
               window_size=SLACK_EMOJI_SIZE, 
@@ -27,11 +37,8 @@ def gif_slice(filepath,
         slices = [[] for _ in range(num_slice_y*num_slice_x)]
 
         for frame in ImageSequence.Iterator(im):
-            for j in range(num_slice_y):
-                for i in range(num_slice_x):
-                    bounds = (i*window_size[0], j*window_size[1], (i+1)*window_size[0], (j+1)*window_size[1])
-                    f = frame.crop(bounds)
-                    slices[i+num_slice_x*j].append(f.resize(output_size))
+            for i, img in enumerate(_get_next_slice(frame, window_size)):
+                slices[i].append(img.resize(output_size))
 
         for i, s in enumerate(slices):
             s[0].save(f'{output_prefix}_{i}{ext_name}', format='GIF', append_images=s[1:-1:skip_rate], save_all=True, optimize=True, duration=d*skip_rate)
